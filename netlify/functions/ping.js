e16cf8ad5498c2311cbc612eacc8b4cb396a958f
Nishant Agrahari <52564870+nishantagrahari@users.noreply.github.com>
2025-09-06 21:47:44 +0530

    updating th ping .js file

--- netlify/functions/ping.js
+++ netlify/functions/ping.js
@@ -1,17 +1,21 @@
 const { createClient } = require('@supabase/supabase-js');
 
-exports.handler = async function(event, context) {
-  const supabaseUrl = process.env.SUPABASE_URL;
+// TEMPORARY: Hardcode your Supabase credentials here
+const supabaseUrl = process.env.SUPABASE_URL;
   const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+  
+exports.handler = async function(event, context) {
   const supabase = createClient(supabaseUrl, supabaseKey);
 
   // Harmless query to keep DB awake
-  let res= await supabase.from('cabins').select('id').limit(1);
+  let res = await supabase.from('cabins').select('id').limit(1);
 
   return {
     statusCode: 200,
-    result:res,
-    body: 'Pinged Supabase!',
+    body: JSON.stringify({
+      result: res,
+      message: 'Pinged Supabase!',
+    }),
   };
 };
 
